9be2bdb9e80a94783d439e98ec9ac568fa42b043
Duan <duanw>
2015-05-04 17:17:26 -0400

    added certs

--- app/models/result.rb
+++ app/models/result.rb
@@ -7,7 +7,7 @@ class Result < ActiveRecord::Base
 	validates_presence_of :ranking_id, :score
 
 	def self.get_result(game_id, user_id)
-		Ranking.where("game_id = :game AND user_id = :user", { game: game_id, user: user_id }).first
+		Ranking.where("game_id = :game AND user_id = :user", { game: game_id, user: user_id }).first.result
 	end
 
     def other_result
