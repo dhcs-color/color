386c9f9ef4c22902bdd2acb566d42fcecebd860c
Duan <duanw>
2015-05-04 16:17:59 -0400

    wrote some functions for result

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,5 +4,13 @@ class Result < ActiveRecord::Base
 	delegate :game, to: :ranking
 
 	validates_presence_of :ranking_id, :score
+
+	def self.get_result(game_id, user_id)
+		Ranking.where("game_id = :game AND user_id = :user", { game: game_id, user: user_id }).first
+	end
+
+	def get_user(user_id)
+		User.find(self.ranking.user_id)
+	end
 	
 end
