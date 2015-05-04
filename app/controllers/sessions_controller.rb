90a30771ad9072c104dce63afb9b04d3adc72d26
Duan <duanw>
2015-05-03 23:08:58 -0400

    hardcoded in fb info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,12 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url
   end
+
+  private
+	def user_params
+	  accessible = [ :name, :email ] # extend with your own params
+	  accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
+	  params.require(:user).permit(accessible)
+	end
+
 end
