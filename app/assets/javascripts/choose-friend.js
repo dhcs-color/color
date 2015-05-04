42b2a3e40827a4c7dfa7b8d24a449c6dbd0062d8
Celine Nguyen <hello@celinenguyen.com>
2015-05-04 09:13:21 -0400

    get rid of octothorpe when submitting hex color rankings

--- app/assets/javascripts/choose-friend.js
+++ app/assets/javascripts/choose-friend.js
@@ -1,11 +1,11 @@
-jQuery(function() {
-  return $('.game-new-choose-friend').click(function() {
-    var name = $(this).parent().attr('data-name');
-    var id = $(this).parent().attr('data-id')
-    console.log(this);
-    $('.friend_chosen').attr('src', $(this).attr('src'));
-    $('.friend_name').html(name);
-    $('#game_to_user_id').val(id);
-    console.log(name);
-  });
-});
+// jQuery(function() {
+//   return $('.game-new-choose-friend').click(function() {
+//     var name = $(this).parent().attr('data-name');
+//     var id = $(this).parent().attr('data-id')
+//     console.log(this);
+//     $('.friend_chosen').attr('src', $(this).attr('src'));
+//     $('.friend_name').html(name);
+//     $('#game_to_user_id').val(id);
+//     console.log(name);
+//   });
+// });
